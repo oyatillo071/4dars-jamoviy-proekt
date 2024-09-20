5a801aa04c58c51190d2c9be830659874866f38b
oyatillo071 <oyatillotoshtemirov071@gmail.com>
2024-09-20 17:06:41 +0000

    fixed mistake in js

--- main.js
+++ main.js
@@ -1,7 +1,24 @@
-window.onload = function() {
-    const loader = document.querySelector('.loader');
-    
-    if (loader) {
-        loader.style.display = 'none';
-    }
-};
+
+window.addEventListener('load', () => {
+    const observer = new PerformanceObserver((list) => {
+        const entries = list.getEntriesByType('resource');
+        let allLoaded = true;
+
+        entries.forEach((entry) => {
+            if (entry.initiatorType === 'img' || entry.initiatorType === 'script' || entry.initiatorType === 'link') {
+                if (!entry.responseEnd) {
+                    allLoaded = false;
+                }
+            }
+        });
+
+        if (allLoaded) {
+            const loader = document.querySelector('.loader');
+            if (loader) {
+                loader.style.display = 'none';
+            }
+        }
+    });
+
+    observer.observe({ entryTypes: ['resource'] });
+});
